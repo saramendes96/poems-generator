09b1af6c6cc32ff262df150f008b9fb1f07d57dc
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-07-18 21:06:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function generatePoem(event) {
   event.preventDefault();
 
+let apiKey = "ac399aato84e379f39f3cfe3ba24af50";
+
 new Typewriter("#poem", {
   strings: "La tombe dit à la rose",
   autoStart: true,
