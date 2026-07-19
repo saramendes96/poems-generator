137f0bc13e8dd6c3d91fc5c39fc6a6da2bd9e228
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-07-19 12:30:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-let apiKey = "ac399aato84e379f39f3cfe3ba24af50";
-
-new Typewriter("#poem", {
+function displayPoem(response) {
+  new Typewriter("#poem", {
   strings: "La tombe dit à la rose",
   autoStart: true,
   delay: 1,
   cursor: "",
 }
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+let apiKey = "ac399aato84e379f39f3cfe3ba24af50";
+let prompt = "";
+let context = "";
+let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}";
+
+axios.get(apiURL).then(displayPoem);
 
 let poemFormElement = document.querySelector ("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
