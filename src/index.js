9f10e2dab0afce75656478b381884096dcc63a4c
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-07-28 22:22:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 function displayPoem(response) {
   console.log("poem generated");
+
   new Typewriter("#poem", {
-  strings: response.data.answer,
-  autoStart: true,
-  delay: 1,
-  cursor: "",
-}
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 function generatePoem(event) {
   event.preventDefault();
 
-let instructionsInput = document.querySelector("#user-instructions");
-let apiKey = "ac399aato84e379f39f3cfe3ba24af50";
-let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with ´SheCodesAI` inside a <strong> element at the end of the poem and not at the beginning";
-let prompt = 'User instructions: Generate a French poem about $"{instructionsInput.value}';
-let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}";
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ac399aato84e379f39f3cfe3ba24af50";
 
-console.log("generating poem");
-console.log('Prompt: ${prompt}`);
-console.log(`Context:${context}´);
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with <strong>SheCodesAI</strong> at the end of the poem.";
 
-axios.get(apiURL).then(displayPoem);
+  let prompt = `User instructions: Generate a French poem about "${instructionsInput.value}"`;
 
-let poemFormElement = document.querySelector ("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
+}
