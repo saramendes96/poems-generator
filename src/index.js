e140744e2c48b9acf02411375156f5cfb8f0deb0
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-08-13 23:41:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateDestination(event) {
   let apiKey = "ac399aato84e379f39f3cfe3ba24af50";
 
   let context =
-  "You are a travel expert who specializes in creating personalized travel recommendations. Your mission is to suggest a travel destination based on the user's instructions. Provide a short and engaging recommendation in basic HTML. Use <br/> to separate different pieces of information. Do NOT wrap the response in ```html or ``` markdown code fences. Do not include a title. Make sure to follow the user instructions.";
+  "You are a travel expert who specializes in creating personalized travel recommendations. Your mission is to suggest a travel destination based on the user's instructions. Provide a short and engaging recommendation in basic HTML. Use <br/> to separate different pieces of information. Do NOT wrap the response in ```html or ``` markdown code fences. Do not include a title. Make sure to follow the user instructions.Sign the response with <strong>SheCodesAI</strong> at the end.";
   
   let prompt = `User instructions: Suggest a travel destination based on "${instructionsInput.value}"`;
   
